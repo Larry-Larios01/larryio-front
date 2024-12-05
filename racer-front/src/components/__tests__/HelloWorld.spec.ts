98cdcc06290022e65880bbb0931e6cac861e07a0
Gerson Gallo <gerson.gallo@leading2lean.com>
2024-12-05 16:45:50 -0600

    racer-front: clean all unnecesary code

--- racer-front/src/components/__tests__/HelloWorld.spec.ts
+++ racer-front/src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import App from '../../App.vue'
 
-describe('HelloWorld', () => {
+describe('App', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(App, { props: {} })
+    expect(wrapper.text()).toContain('Hello world')
   })
 })
