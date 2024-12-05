861084219c41aafd3f2592458a1097053fb46da5
Gerson Gallo <gerson.gallo@leading2lean.com>
2024-12-05 17:01:35 -0600

    feat(racer-front): created Competition

--- racer-front/src/App.vue
+++ racer-front/src/App.vue
@@ -1,15 +1,24 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue"
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Competition from "./components/Competition.vue"
+import type { Player } from "./components/Competition.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    Competition
+  },
   setup() {
     const hello = ref("world")
+    const players = ref<Player[]>([{
+      name: "Larry"
+    }, {
+      name: "Gerson"
+    }])
 
     return {
-      hello
+      hello,
+      players,
     }
   }
 })
@@ -21,6 +30,7 @@ export default defineComponent({
 
   <main>
     <div>Hello {{ hello }}</div>
+    <Competition v-bind:laps-count="6" v-bind:players="players"></Competition>
   </main>
 </template>
 
---
+++ racer-front/src/components/Competition.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+import { defineComponent, ref } from "vue"
+import type { PropType } from "vue";
+
+export type Player = {
+    name: string
+}
+
+export default defineComponent({
+    name: "Competition",
+    props: {
+        players: {
+            type: Object as PropType<Player[]>,
+            required: true,
+        },
+        lapsCount: {
+            type: Number,
+            required: true
+        }
+    },
+    setup(props, ctx) {
+
+    }
+})
+
+</script>
+
+<template>
+
+<div>
+    Players:
+    <ul v-for="player in players">
+        <li>{{ player.name }}</li>
+    </ul>
+</div>
+<div>
+    Laps: {{  lapsCount }}
+</div>
+
+</template>
