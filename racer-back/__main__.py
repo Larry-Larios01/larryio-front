58db682546e8042c5e00a60112e45dc42fb41759
larry123 <larioslarry8@gmail.com>
2024-12-20 09:48:15 -0600

    adding the part of the backend

--- racer-back/__main__.py
+++ racer-back/__main__.py
@@ -11,7 +11,7 @@ import psycopg
 from psycopg.rows import dict_row
 from psycopg import AsyncRawCursor
 from starlette.middleware.cors import CORSMiddleware
-
+from starlette.middleware import Middleware
 
 
 load_dotenv(verbose=True)
@@ -33,6 +33,7 @@ class User(BaseModel):
 class Competition(BaseModel):
     id: UUID4
     name: str
+    laps: int
     players: list[UUID4]
 
 
@@ -114,19 +115,19 @@ async def from_req_insert_competition(request: Request)-> Competition:
     data =  await request.json()
     players = [uuid.UUID(player) for player in data["players"]]
     print(players)
-    competition = Competition(id=uuid4,name=data["name"], players=players )
+    competition = Competition(id=uuid4,name=data["name"], players=players, laps=data["laps"] )
     return competition
 
 async def insert_competition_handler(params: Competition, conn)-> uuid:
    
     async with conn.cursor() as c:
             result = await c.execute(
-                "INSERT INTO competition (id, name) VALUES ($1, $2) RETURNING id, name",
-                [params.id, params.name]
+                "INSERT INTO competition (id, name, laps) VALUES ($1, $2, $3) RETURNING id, name, laps",
+                [params.id, params.name, params.laps]
                     )
             comp = await result.fetchone()
             print(comp)
-            competition = Competition(id=comp["id"],name=comp['name'], players=params.players)
+            competition = Competition(id=comp["id"],name=comp['name'], players=params.players, laps=comp["laps"])
 
             for user in params.players:
                   await c.execute(
@@ -164,14 +165,14 @@ async def get_competitions_handler(conn)-> list[Competition]:
                 players = await conn.execute("SELECT cm.id_player FROM competition c INNER JOIN competitionMaster cm ON c.id = cm.id_competition WHERE c.id = $1;", [comp["id"]])
                 players_get = await players.fetchall()
                 players_send = [row["id_player"] for row in players_get]
-                competition = Competition(id=comp["id"],name=comp["name"], players=players_send)
+                competition = Competition(id=comp["id"],name=comp["name"], players=players_send, laps=comp["laps"])
                 competitions.append(competition)      
     await conn.close()
     return competitions
 
 
 def to_res_get_competitions(comps: list[Competition])-> JSONResponse:
-     user_serialized = [comp.model_dump_json(indent=2) for comp in comps]
+     user_serialized = [comp.model_dump(mode="json") for comp in comps]
      print(user_serialized)
      return JSONResponse(user_serialized)
 
@@ -183,30 +184,62 @@ async def get_competitions(request: Request):
     return to_res_get_competitions(result)
 
 
+#the begin of the logic to get one user
+def from_req_get_user(request: Request)-> uuid.UUID:
+    user_id = request.path_params.get("id")
+    uuid_obj = uuid.UUID(user_id)
+    print("the id of param",uuid_obj)
+    return uuid_obj
+
+
+async def get_user_handler(params: uuid.UUID, conn)-> User:
+    async with conn.transaction():
+            restult = await conn.execute("SELECT * FROM users WHERE id = $1",[params])
+            user = await restult.fetchone()
+            contact = User(id=user["id"],name=user["name"])
+    await conn.close()
+    return contact
+
+
+def to_res_get_user(player: User)-> JSONResponse:
+     user_serialized = player.model_dump(mode="json")
+     return JSONResponse(user_serialized)
+
+
+
+async def get_user(request: Request):
+    conn = await get_connection()
+    params = from_req_get_user(request)
+    result = await get_user_handler(params, conn)
+    return to_res_get_user(result)
+
+
+
         
 
 
 
 
 routes = [
-    Route("/Player/", endpoint=insert_user, methods=["POST"]),
-    Route("/Player/", endpoint=get_users, methods=["GET"]),
-    Route("/Competition/", endpoint=insert_competition, methods=["POST"]),
-    Route("/Competition/", endpoint=get_competitions, methods=["GET"]),
+    Route("/player/", endpoint=insert_user, methods=["POST"]),
+    Route("/player/", endpoint=get_users, methods=["GET"]),
+    Route("/player/{id}", endpoint=get_user, methods=["GET"]),
+    Route("/competition/", endpoint=insert_competition, methods=["POST"]),
+    Route("/competition/", endpoint=get_competitions, methods=["GET"]),
+]
+
+
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=["*"],allow_methods=["*"],  allow_headers=["*"])
 ]
 
 
 
 app = Starlette(
-    routes=routes
+    routes=routes,
+    middleware=middleware
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Permitir cualquier origen, en producción reemplaza con tu dominio específico
-    allow_methods=["*"],  # Permitir todos los métodos: POST, GET, OPTIONS, etc.
-    allow_headers=["*"],  # Permitir todos los headers
-)
 
 
 
